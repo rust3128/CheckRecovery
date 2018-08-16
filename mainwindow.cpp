ff62f3bf4bcd5848cd41b2db7b4160bd0caeaf7a
rust3128 <rust3128@gmail.com>
2018-08-16 15:20:51 +0300

    1233

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,8 +9,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tcpSocket = new QTcpSocket(this);
     ui->mainToolBar->hide();
     ui->menuRec->setEnabled(false);
+    createStatusBar();
+    ui->groupBoxTerminal->hide();
     connect(this,&MainWindow::sigShowEvent,this,&MainWindow::slotShowEvent,Qt::ConnectionType(Qt::QueuedConnection | Qt::UniqueConnection));
 }
 
@@ -28,6 +31,11 @@ void MainWindow::slotShowEvent()
     if(connCentralDB(connID)){
         ui->mainToolBar->show();
         ui->menuRec->setEnabled(true);
+        setModelTerminals();
+        ui->groupBoxTerminal->show();
+        ui->labelTerminlInfo->setText("Терминал не указан!");
+        ui->labelOnLineStatus->hide();
+        ui->line->hide();
     }
 }
 
@@ -65,6 +73,7 @@ bool MainWindow::connCentralDB(int connID)
     QSqlDatabase dbcentr = QSqlDatabase::addDatabase("QIBASE","central");
 
     dbcentr.setHostName(modelConnect->data(modelConnect->index(connID,2)).toString());
+//    dbcentr.setHostName("192.168.200.22");
     dbcentr.setDatabaseName(modelConnect->data(modelConnect->index(connID,3)).toString());
     dbcentr.setUserName(modelConnect->data(modelConnect->index(connID,4)).toString());
     dbcentr.setPassword(modelConnect->data(modelConnect->index(connID,5)).toString());
@@ -76,20 +85,141 @@ bool MainWindow::connCentralDB(int connID)
                 .arg(dbcentr.lastError().text()),
                 QMessageBox::Ok);
         strStatus = "Ошибка подключения к центральной базе!";
+        statusLabel->setStyleSheet("color: rgb(255, 0, 0)"
+                                   "border-style: solid;"
+                                   "border-width: 1px;"
+                                   "border-color: black;");
         result =false;
 
     } else {
         strStatus = "Подключено к базе данных: "+modelConnect->data(modelConnect->index(connID,1)).toString()+" "
                 +modelConnect->data(modelConnect->index(connID,2)).toString()+":"
                 +modelConnect->data(modelConnect->index(connID,3)).toString();
+        statusLabel->setStyleSheet("color: rgb(0, 85, 0);"
+                                   "border-style: solid;"
+                                   "border-width: 1px;"
+                                   "border-color: black;");
         result=true;
     }
-    ui->statusBar->showMessage(strStatus);
+    statusLabel->setText(strStatus);
     return result;
 
 }
 
+void MainWindow::createStatusBar()
+{
+    statusLabel = new QLabel(this);
+    ui->statusBar->addPermanentWidget(statusLabel);
+}
+
+bool MainWindow::validateAZS(int idx)
+{
+    ui->labelOnLineStatus->show();
+
+    tcpSocket->abort();
+    tcpSocket->connectToHost(modelTerminals->data(modelTerminals->index(idx,2)).toString(),3050);
+    if(tcpSocket->waitForConnected(30000)){
+        qInfo(logInfo()) << "Проверка доступности. Сервер: " << modelTerminals->data(modelTerminals->index(idx,2)).toString() <<  "FireBird доступен.";
+        ui->labelOnLineStatus->setStyleSheet("color: rgb(0, 170, 0);font: 75 14pt 'Noto Sans'");
+        ui->labelOnLineStatus->setText("АЗС на связи!");
+        ui->line->show();
+        return true;
+    } else {
+        qInfo(logInfo()) << "Проверка доступности. Сервер: " << modelTerminals->data(modelTerminals->index(idx,2)).toString() <<  "FireBird НЕ доступен.";
+        ui->labelOnLineStatus->setStyleSheet("color: red;font: 75 14pt 'Noto Sans'");
+        ui->labelOnLineStatus->setText("Отсутствует связь с АЗС!");
+        ui->line->show();
+        return false;
+    }
+
+}
+
+void MainWindow::initLostCheckFuels()
+{
+    lostCheck.insert("TERMINAL_ID",0);
+    lostCheck.insert("SHIFT_ID",0);
+    lostCheck.insert("DISPENSER_ID",-1);
+    lostCheck.insert("TRK_ID",0);
+    lostCheck.insert("TANK_ID",0);
+    lostCheck.insert("FUEL_ID",-1);
+    lostCheck.insert("GIVE",0);
+    lostCheck.insert("ORDERED",0);
+    lostCheck.insert("SUMMA",0);
+    lostCheck.insert("CASH",0);
+    lostCheck.insert("DISCOUNTSUMMA",0);
+    lostCheck.insert("PAYTYPE_ID",-1);
+    lostCheck.insert("NUM_CHECK",0);
+    lostCheck.insert("NUM_CHECK_RETURN",0);
+    lostCheck.insert("TRANSACTION_ID",0);
+    lostCheck.insert("SEC",0);
+    lostCheck.insert("ISLAST","T");
+    lostCheck.insert("INFO_CODE",-1);
+    lostCheck.insert("INFO_TEXT","");
+    lostCheck.insert("POS_ID",1);
+    lostCheck.insert("ZNUMBER",0);
+    lostCheck.insert("OPERATOR_ID",-1);
+    lostCheck.insert("SALEORDER_ID","GEN_ID(GEN_SALEORDERS, 1)");
+    lostCheck.insert("PRICE",0);
+    lostCheck.insert("ISBEFOREPAY","F");
+    lostCheck.insert("POSTRANSACTION_ID",0);
+    lostCheck.insert("POSTRNRETURN_ID",0);
+    lostCheck.insert("SHARE_ID",0);
+    lostCheck.insert("MPOSCHECK_ID",":CHECK_ID");
+    lostCheck.insert("PAYTYPE_ID2",1);
+    lostCheck.insert("SUMMA2",0.00);
+    lostCheck.insert("DISCOUNTSUMMA2",0.00);
+    lostCheck.insert("DAT",0);
+    lostCheck.insert("GOV_NUMBER","");
+    lostCheck.insert("BONUSCARD","");
+}
+
+void MainWindow::setModelTerminals()
+{
+    QSqlDatabase dbcenter = QSqlDatabase::database("central");
+    modelTerminals = new QSqlQueryModel();
+    modelTerminals->setQuery("SELECT t.terminal_id, TRIM(t.name) AS name, c.SERVER_NAME, c.DB_NAME, c.CON_PASSWORD FROM TERMINALS t "
+                             "LEFT JOIN CONNECTIONS c ON t.TERMINAL_ID = c.TERMINAL_ID "
+                             "WHERE t.TERMINALTYPE=3 and t.ISACTIVE='T' and c.CONNECT_ID=2 "
+                             "ORDER BY t.TERMINAL_ID",dbcenter);
+}
+
 void MainWindow::on_action_triggered()
 {
+
+}
+
+void MainWindow::on_lineEditTerminal_textChanged(const QString &arg1)
+{
+    if(arg1.length()<4)
+    {
+        ui->labelTerminlInfo->setText("Терминал не указан!");
+    }
+    else
+    {
+        int terminal = arg1.toInt();
+        int idx = -1;
+        QString strTermInfo = "Не верный номер терминала!";
+        for(int i=0;i<modelTerminals->rowCount();++i)
+        {
+            if(terminal == modelTerminals->data(modelTerminals->index(i,0)).toInt())
+            {
+                idx=i;
+                break;
+            }
+        }
+        if(idx>=0)
+        {
+            strTermInfo=modelTerminals->data(modelTerminals->index(idx,1)).toString();
+            currentTerminal=terminal;
+            validateAZS(idx);
+
+        }
+        else
+        {
+            ui->label->hide();
+            ui->labelOnLineStatus->hide();
+        }
+        ui->labelTerminlInfo->setText(strTermInfo);
+    }
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include <QSqlTableModel>
+#include <QSqlQueryModel>
+#include <QLabel>
+#include <QTcpSocket>
 
 namespace Ui {
 class MainWindow;
@@ -16,10 +19,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-
 private slots:
     void slotShowEvent();
     void on_action_triggered();
+
+    void on_lineEditTerminal_textChanged(const QString &arg1);
 
 signals:
     void sigShowEvent();
@@ -27,11 +31,21 @@ protected:
     void showEvent(QShowEvent *ev);
 private:
     void setModelConnect();             //Создание модели списка соединений
+    void setModelTerminals();           //Создание модели списка терминалов
+    void initLostCheckFuels();
     int selectCentralDB();              //получение текущего подключения
     bool connCentralDB(int connID);     //Подключение к ЦБ
+    void createStatusBar();             //Создание строки состогяния
+    bool validateAZS(int idx);                 //Проверка доступности АЗС
 private:
     Ui::MainWindow *ui;
-    QSqlTableModel *modelConnect;       //Модель списка соединений
+    QSqlTableModel *modelConnect;       //Модель списка соединений c центральной базо
+    QSqlQueryModel *modelTerminals;     //Модель списка терминалов
+    QLabel *statusLabel;                //Информация о текщем подключении
+    QMap<QString, QVariant> lostCheck;  //Контейнер данных для формирования скрипта
+    int currentTerminal;                //Выбранный терминал
+    QTcpSocket  *tcpSocket;             //Проверка доступности FierBird
+
 };
 
 #endif // MAINWINDOW_H
